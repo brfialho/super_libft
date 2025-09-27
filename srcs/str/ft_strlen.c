2bc39dbd63f2f6f7a826a2fe1792f32194b42d0f
brfialho <brfialho@student.42sp.org.br>
2025-09-26 23:13:42 -0300

    updated strlen

--- srcs/str/ft_strlen.c
+++ srcs/str/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 23:19:13 by brfialho          #+#    #+#             */
-/*   Updated: 2025/09/26 16:57:46 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/09/26 23:12:57 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
 	while (*s++)
 		len++;
