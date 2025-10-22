6ea398a4c6d4e3efdbd3ae11eaf7ab0b3a834ac9
brfialho <brfialho@student.42sp.org.br>
2025-10-22 20:39:42 -0300

    changed iterator string char to const char ft_str_allinset

--- srcs/str/ft_str_allinset.c
+++ srcs/str/ft_str_allinset.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 19:26:55 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/22 19:28:42 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/22 20:38:50 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_str_allinset(const char *string, const char *set)
 {
-	char	*s;
+	const char	*s;
 
-	s = (char *)string;
+	s = string;
 	while (*s)
 		if (!ft_strchr(set, *s++))
 			return (NULL);
