7b3c7e9855de20a875d81efed4c29b2f0d553edc
brfialho <brfialho@student.42sp.org.br>
2025-10-23 15:05:44 -0300

    changed tab inits to work with null ending

--- srcs/matrix/ft_tab_init_alloc.c
+++ srcs/matrix/ft_tab_init_alloc.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 14:26:42 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/23 13:20:09 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/23 15:04:45 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ t_tab	*ft_tab_init_alloc(t_tab *tab, size_t rows, size_t cols, size_t e_size)
 
 	tab->element_size = e_size;
 	tab->cols = cols;
-	tab->tab = ft_calloc(rows, sizeof(void *));
+	tab->tab = ft_calloc(rows + 1, sizeof(void *));
 	if (!tab->tab)
 		return (NULL);
 	i = 0;
 	while (i < rows)
 	{
 		tab->rows = i;
-		tab->tab[i] = ft_calloc(cols, e_size);
+		tab->tab[i] = ft_calloc(cols + 1, e_size);
 		if (!tab->tab[i])
 			return (ft_tab_free_content(tab), NULL);
 		i++;
--- srcs/matrix/ft_tab_init_empty.c
+++ srcs/matrix/ft_tab_init_empty.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 14:27:14 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/22 14:28:58 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/10/23 15:04:46 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_tab	*ft_tab_init_empty(t_tab *tab, size_t rows, size_t cols, size_t e_size)
 	tab->rows = rows;
 	tab->cols = cols;
 	tab->element_size = e_size;
-	tab->tab = ft_calloc(rows, sizeof(void *));
+	tab->tab = ft_calloc(rows + 1, sizeof(void *));
 	if (!tab->tab)
 		return (NULL);
 	return (tab);
