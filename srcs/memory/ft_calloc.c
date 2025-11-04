afce506373c7c3234e2193bbea7ead33ee0fadc6
brfialho <brfialho@student.42sp.org.br>
2025-11-04 18:39:43 -0300

    updated calloc return value

--- srcs/memory/ft_calloc.c
+++ srcs/memory/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 04:56:47 by brfialho          #+#    #+#             */
-/*   Updated: 2025/10/18 18:10:09 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/11/04 18:35:55 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*array;
 
 	if (size && nmemb > ((size_t) -1) / size)
-		return (0);
+		return (NULL);
 	array = malloc(nmemb * size);
 	if (!array)
-		return (0);
+		return (NULL);
 	ft_bzero(array, nmemb * size);
 	return (array);
 }
